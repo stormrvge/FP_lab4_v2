1703d2470f1e8d0985d2c3495008c5921bf69979
Andrey Svistukhin <a.svistukhin@just-ai.com>
2023-03-24 07:16:23 +0300

    test

--- test/test.py
+++ test/test.py
@@ -5,10 +5,10 @@ import time
 
 def main():
     # Start an Erlang shell
-    erl_process_discovery = subprocess.Popen(['erl', '-sname', 'discovery'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    erl_process_alice = subprocess.Popen(['erl', '-sname', 'alice'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    erl_process_bob = subprocess.Popen(['erl', '-sname', 'bob'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    erl_process_charlie = subprocess.Popen(['erl', '-sname', 'charlie'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    erl_process_discovery = subprocess.Popen(['erl', '-sname', 'discovery', "-cookies", "1234"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    erl_process_alice = subprocess.Popen(['erl', '-sname', 'alice', "-cookies", "1234"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    erl_process_bob = subprocess.Popen(['erl', '-sname', 'bob', "-cookies", "1234"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    erl_process_charlie = subprocess.Popen(['erl', '-sname', 'charlie', "-cookies", "1234"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     init_nodes(erl_process_discovery, erl_process_alice, erl_process_bob, erl_process_charlie)
     time.sleep(1)
