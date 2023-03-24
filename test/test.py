c8f6f1b28928811deae3e03924e92cc458a53301
Andrey Svistukhin <a.svistukhin@just-ai.com>
2023-03-24 11:52:21 +0300

    test

--- test/test.py
+++ test/test.py
@@ -1,7 +1,8 @@
 import subprocess
 import time
 
-def test_chat_server():
+
+def main():
     chat_server_process = subprocess.Popen(["erl", "-sname", "test_server", "-run", "chat_server", "start"])
     time.sleep(5)  # Give the server some time to start
 
@@ -19,3 +20,7 @@ def test_chat_server():
         assert "ok" in send_message.stdout, "Sending a message to the chat server failed"
     finally:
         chat_server_process.terminate()
+
+
+if __name__ == "__main__":
+    main()
