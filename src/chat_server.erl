80398d94dd1b83a4078fda46f048fc661db32812
Andrey Svistukhin <a.svistukhin@just-ai.com>
2023-03-24 05:03:21 +0300

    init logic

---
+++ src/chat_server.erl
@@ -0,0 +1,175 @@
+-module(chat_server).
+-behaviour(gen_server).
+
+%% API
+-export([start/0, send/2, accept/3, refresh_connections/2, get_connections/1,  handshake/1, ping/1, print_history/1, get_messages/1, leave/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {connections = [], ets = undefined, public_key = undefined, private_key = undefined}).
+
+start() ->
+  gen_server:start_link(?MODULE, [], []).
+
+send(User, Message) ->
+  gen_server:cast(User, {send, User, Message}).
+
+refresh_connections(User, Connections) ->
+  gen_server:cast(User, {refresh_connections, Connections}).
+
+handshake(User) ->
+  gen_server:call(User, {handshake}).
+
+accept(Sender, User, Message) ->
+  gen_server:cast(User, {chat_message, Sender, Message}).
+
+get_messages(User) ->
+  gen_server:call(User, get_messages).
+
+get_connections(User) ->
+  gen_server:call(User, get_connections).
+
+print_history(User) ->
+  gen_server:cast(User, print_history).
+
+ping(User) -> gen_server:call(User, ping).
+
+leave(User) ->
+  gen_server:call(?MODULE, {leave, User}).
+
+%% gen_server callbacks
+init([]) ->
+  {PublicKey, PrivateKey} = crypto:generate_key(rsa, {1024, 3}),
+
+  NewEtsTable = ets:new(messages, [public, named_table, {keypos, 2}, duplicate_bag]),
+
+  try
+    {Connections, EtsTableList} = discovery_server:register(self()),
+    case EtsTableList of 
+      undefined ->
+        {ok, #state{connections = Connections, ets = NewEtsTable, public_key = PublicKey, private_key = PrivateKey}};
+      _ ->
+        lists:foreach(
+          fun(Object) ->
+            ets:insert(NewEtsTable, Object)
+          end, 
+          EtsTableList
+        ),
+        {ok, #state{connections = Connections, ets = NewEtsTable, public_key = PublicKey, private_key = PrivateKey}}
+    end
+  catch 
+    _:_ ->
+      Pids = read_pids(),
+      User = find_available_user(Pids),
+      NewConnections = chat_server:get_connections(User),
+      EtsList = chat_server:get_messages(User),
+      lists:foreach(
+          fun(Object) ->
+            ets:insert(NewEtsTable, Object)
+          end, 
+          EtsList
+      ),
+      {ok, #state{connections = NewConnections, ets = NewEtsTable, public_key = PublicKey, private_key = PrivateKey}}
+  end.
+
+handle_call(get_messages, _From, State) ->
+  {reply, ets:tab2list(State#state.ets), State};
+
+handle_call(get_connections, _From, State) ->
+  {reply, State#state.connections, State};
+
+handle_call(ping, _From, State) ->
+  {reply, {ok, "Connection can be established"}, State};
+
+handle_call({leave, User}, _From, State) ->
+  save_data(State#state.connections),
+  {reply, ok, State#state{connections = lists:delete(User, State#state.connections)}};
+
+handle_call({handshake}, _From, #state{connections = Users, ets = Ets, public_key = PublicKey, private_key = PrivateKey}) ->
+  {reply, PublicKey, #state{connections = Users, ets = Ets, public_key = PublicKey, private_key = PrivateKey}}.
+
+handle_cast({send, User, Message}, State) ->
+  Timestamp = erlang:system_time(millisecond),
+  ets:insert_new(State#state.ets, {{User, Timestamp}, Message}),
+  send_to_all(User, Message, State#state.connections),
+  {noreply, State};
+
+handle_cast({chat_message, From, EncryptedMessage}, State) ->
+  DecryptedMessage = crypto:private_decrypt(rsa, EncryptedMessage, State#state.private_key, []),
+  Timestamp = erlang:system_time(millisecond),
+  ets:insert_new(State#state.ets, {{From, Timestamp}, DecryptedMessage}),
+  io:format("User received: From: ~w Message: ~s~n", [From, binary_to_list(DecryptedMessage)]),
+  {noreply, State};
+
+handle_cast({refresh_connections, Connections}, #state{connections = _, ets = Ets, public_key = PublicKey, private_key = PrivateKey}) ->
+  {noreply, #state{connections = Connections, ets = Ets, public_key = PublicKey, private_key = PrivateKey}};
+
+handle_cast(print_history, State) ->
+  EtsList = ets:tab2list(State#state.ets),
+
+  io:format("The history~n"),
+
+  lists:foreach(
+    fun({{From, Timestamp}, Message}) ->
+      UTCTime = calendar:system_time_to_universal_time(Timestamp, millisecond),
+      LocalTime = calendar:universal_time_to_local_time(UTCTime),
+      FormattedTime = format_time(LocalTime),
+      io:format("[~s] From ~w Message ~s~n", [FormattedTime, From, Message])
+    end,
+    EtsList
+  ),
+
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%% Helper functions
+send_to_all(User, Message, Users) ->
+  lists:foreach(fun(Other) -> 
+    %Логику проверки произведен ли уже обмен ключами, если да  то зачем еще раз обмениваться для этого нужно создать map где ключ pid, значение - public_key
+    UserPublicKey = chat_server:handshake(Other),
+    EncryptedMessage = crypto:public_encrypt(rsa, list_to_binary(Message), UserPublicKey, []),
+    chat_server:accept(User, Other, EncryptedMessage) 
+    end, 
+    Users
+  ).
+
+format_time({{Year, Month, Day}, {Hour, Minute, Second}}) ->
+  io_lib:format("~4.10.0B-~2.10.0B-~2.10.0B-~2.10.0B-~2.10.0B-~2.10.0B", [Year, Month, Day, Hour, Minute, Second]).
+
+read_pids() ->
+  {ok, Fd} = file:open("peers", [read]),
+  try get_all_lines(Fd, []) after file:close(Fd) end.
+
+
+get_all_lines(Device, Lines) ->
+  case io:get_line(Device, "") of
+    eof -> Lines;
+    Line -> get_all_lines(Device, [string:chomp(Line) | Lines])
+  end.
+
+find_available_user([]) -> {error, "Unable to find avaible user"};
+
+find_available_user(Pids) ->
+  StringPid = lists:nth(rand:uniform(length(Pids)), Pids),
+  Pid = erlang:list_to_pid(StringPid),
+  try chat_server:ping(Pid) of
+    {ok, _} -> Pid
+  catch
+    _:_ -> find_available_user(lists:delete(StringPid, Pids))
+  end.
+
+save_data(Connections) ->
+  {ok, Fd} = file:open("peers", [write]),
+  lists:foreach(
+    fun (Connection) -> file:write(Fd, io_lib:fwrite("~p\n", [Connection])) end,
+    Connections
+  ).
---
+++ src/discovery_server.erl
@@ -0,0 +1,64 @@
+-module(discovery_server).
+-behaviour(gen_server).
+
+%% API
+-export([start/0, register/1, unregister/1, list/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {users = []}).
+
+start() ->
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%вместо discovery на гитхабе node()
+register(User) ->
+  gen_server:call({?MODULE, 'discovery@127.0.0.1'}, {register, User}).
+
+unregister(User) ->
+  gen_server:call({?MODULE, 'discovery@127.0.0.1'}, {unregister, User}).
+
+list() ->
+  gen_server:call({?MODULE, 'discovery@127.0.0.1'}, list).
+
+%% gen_server callbacks
+init([]) ->
+  {ok, #state{users = []}}.
+
+handle_call({register, User}, _From, State) ->
+  NewUsers = [User | State#state.users],
+  %обновить лист подключений у всех пиров
+
+  lists:foreach(
+    fun(Connection) ->
+      chat_server:refresh_connections(Connection, lists:delete(Connection, NewUsers))
+    end,
+    State#state.users
+  ), 
+
+  case State#state.users of 
+    [] -> {reply, {State#state.users, undefined}, State#state{users = NewUsers}};
+    _ ->
+      RandomUser = lists:nth(rand:uniform(length(State#state.users)), State#state.users),
+      ChatHistory = chat_server:get_messages(RandomUser),
+      {reply, {State#state.users, ChatHistory}, State#state{users = NewUsers}}
+  end;
+
+handle_call({unregister, User}, _From, State) ->
+  {reply, ok, State#state{users = lists:delete(User, State#state.users)}};
+
+handle_call(list, _From, State) ->
+  {reply, State#state.users, State}.
+
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+terminate(_Reason, _State) ->
+  ok.
+
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
